471951c6e1c881bb1fdec72b44df59f8a16ec985
Lipexq <77757828+Lipexq@users.noreply.github.com>
2022-04-27 10:34:38 -0300

    Add files via upload

---
+++ Calculadora/src/js/index.js
@@ -0,0 +1,21 @@
+function insert(num){
+   let numberJs =  document.getElementById('result').innerHTML;
+   document.getElementById('result').innerHTML = numberJs + num;
+};
+
+function clean(){
+    document.getElementById('result').innerHTML = "";
+}
+
+function back(){
+    let deletJs = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = deletJs.substring(0, deletJs.length -1)
+}
+
+function result(){
+    let resultJs = document.getElementById('result').innerHTML;
+    
+    if(resultJs){
+         document.getElementById('result').innerHTML = eval(resultJs);
+    }
+}
